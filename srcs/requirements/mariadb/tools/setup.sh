071f3b3e050dd43f84bcc67742647bcc93705ed5
5N0023 <lektaibim@gmail.com>
2023-12-25 11:22:09 -0500

    done

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Ensure necessary directories exist
 mkdir -p /var/run/mysqld
 mkdir -p /run/mysqld
 
@@ -8,25 +7,21 @@ if [ ! -d /var/lib/mysql/mysql ]; then
     mysql_install_db --user=root
 fi
 
-# Start MariaDB
 service mariadb start
 
-# Wait for MariaDB to start
 while ! mysqladmin ping --silent; do
     sleep 1
 done
 
-# Check if the database exists before creating
 if ! mysql -uroot --password=$MYSQL_ROOT_PASSWORD -e "USE inception;" 2>/dev/null; then
-   echo "use mysql;CREATE DATABASE ${MYSQL_DB};CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';FLUSH PRIVILEGES;ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"| mysql -uroot
+   echo "use mysql;CREATE DATABASE ${MYSQL_DB};CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"| mysql -uroot --skip-password
 fi
 
-# service mariadb stop
+kill -9 $(pgrep mariadb)
+
 
 while mysqladmin ping --silent; do
     sleep 1
 done
 
-# Start all the services
-exec mysqld_safe 
-# Start all the services`
+exec mysqld_safe -uroot -p${MYSQL_ROOT_PASSWORD}
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,9 +6,20 @@ sleep 4
 
 wp config create --dbname=${MYSQL_DB} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASS} --dbhost=${WORDPRESS_DB_HOST} --allow-root
 
+# wp plugin install redis-cache --activate --allow-root
+
 wp core install --url=${DOMAIN_NAME} --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --allow-root
 
 wp user create ${WORDPRESS_USER} ${WORDPRESS_USER_EMAIL} --role=${WORDPRESS_USER_ROLE} --user_pass=${WORDPRESS_USER_PASSWORD} --allow-root
+
+# redis
+
+wp config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
+wp config set WP_REDIS_PORT 6379 --raw --allow-root
+wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+wp config set WP_REDIS_CLIENT phpredis --allow-root
+wp plugin install redis-cache --activate --allow-root
+wp plugin update --all --allow-root
 
 wp theme install gutenify-virtual-reality --activate --allow-root
 
