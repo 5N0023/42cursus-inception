2908558c9a1f24acace4a4fc3ca74c39586694ee
5N0023 <lektaibim@gmail.com>
2024-02-01 02:47:46 -0500

    fix some cert params

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -5,7 +5,7 @@ mkdir -p /etc/nginx/ssl/private/ /etc/nginx/ssl/certs/
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout "${CERTS_key}" \
     -out "${CERTS_certs}" \
-    -subj "/C=MA/ST=KH/L=Khouribga/O=1337/OU=1337al Unit/CN=home"
+    -subj "/C=MA/L=Khouribga/O=1337"
 chmod 600 ${CERTS_key} ${CERTS_certs}
 # Start NGINX
 exec nginx -g 'daemon off;'
